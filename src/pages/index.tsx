00df655e84a89d4648ab8959d30f6f4f925406aa
davidp799 <papdav12@gmail.com>
2023-03-23 13:11:43 -0400

    add index functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+function Home() {
   return (
     <div>Hello world!</div>
   )
 }
+
+export default withAuthenticator(Home);
