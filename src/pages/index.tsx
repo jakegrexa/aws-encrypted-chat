3df0b88d26f3ac07e2648b1ebecdd807ab6ab6b8
davidp799 <papdav12@gmail.com>
2023-03-23 13:14:51 -0400

    create form for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-
-const inter = Inter({ subsets: ['latin'] })
+import { listMessages } from "../../graphql/queries"
+import { createMessage } from "../../graphql/mutations";
+import { onCreateMessage } from "../../graphql/subscriptions";
 
 function Home() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target[0].value);
+  };
+
   return (
-    <div>Hello world!</div>
-  )
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <h1 className={styles.title}> AWS Amplify Live Chat</h1>
+
+        <div className={styles.chatbox}>
+          <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit} className={styles.formBase}>
+              <input
+                type="text"
+                id="message"
+                name="message"
+                autoFocus
+                required
+                placeholder="💬 Send a message to the world 🌎"
+                className={styles.textBox}
+              />
+              <button style={{ marginLeft: "8px" }}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default withAuthenticator(Home);
